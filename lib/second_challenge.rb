9f2cb8cca2814eb7de4d068b0ec447483ccd7265
Kachel Wittig <kjwittig@gmail.com>
2018-03-19 23:43:09 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,31 +1,29 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
+epic_tragedy = {
+  :montague => {
+    :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53"
+  },
     :matriarch => {
       :name => "Lady Montague",
       :age => "54"
-    },
+  },
     :hero => {
       :name => "Romeo",
       :age => "15"
-    },
+  },
     :hero_friends => [
-     {
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
-     },
-     {
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
+      {
         :name => "Mercutio",
         :age => "18",
         :attitude => "hot-headed"
       }
     ]
-
   },
   :capulet => {
     :patriarch => {
@@ -53,7 +51,5 @@ def second_challenge
         :attitude => "worried"
       }
     ]
-  }}
-
-
-end
+  }
+}
