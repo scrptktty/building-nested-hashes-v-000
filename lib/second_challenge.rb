659334dfdbbe8a3cbabe249965fa40f3558869f6
Kachel Wittig <kjwittig@gmail.com>
2018-03-19 23:41:12 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,11 +8,52 @@ def second_challenge
     :matriarch => {
       :name => "Lady Montague",
       :age => "54"
-    }
-    }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+    },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+    },
+    :hero_friends => [
+     {
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
+     },
+     {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+    ]
 
-  
+  },
+  :capulet => {
+    :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }}
+
+
 end
