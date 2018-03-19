0ee91b59c00338617a6caf73be7baeb4a3e7965b
Kachel Wittig <kjwittig@gmail.com>
2018-03-19 23:36:39 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+    }
+    }, 
     :capulet => {
       #your key/value pairs here!
       }}
