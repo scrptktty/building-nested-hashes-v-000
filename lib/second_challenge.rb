f2e337f185ef999b878ba061b3eef0f30702789b
Kachel Wittig <kjwittig@gmail.com>
2018-03-19 23:55:24 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,12 @@
 epic_tragedy = {
   :montague => {
+    :patriarch => {},
     :matriarch => {},
     :hero => {},
     :hero_friends => []
   },
   :capulet => {
+    :patriarch => {},
     :matriarch => {},
     :heroine => {},
     :heroine_friends => []
